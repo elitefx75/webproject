941f346b6a1e78ce2da7a95108ff298366af40f1
elitefx75 <pbwire@byupathway.edu>
2026-04-14 12:26:51 +0300

    webproject

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/",
+
   build: {
-    outDir: 'dist', // output folder at WEBPROJECT/dist
-    rollupOptions: {
-      input: {
-        // root entry
-        main: resolve(__dirname, 'index.html'),
-        // if you add more HTML entry points later, list them here
-      },
-    },
+    outDir: "dist",
   },
 });
